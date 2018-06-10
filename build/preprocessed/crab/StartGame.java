98a51ee74a08f8852f88deb2fb81d69072effa91
Kroos <m.gajewski810@gmail.com>
2018-06-10 13:17:54 +0200

    first commit

---
+++ build/preprocessed/crab/StartGame.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crab;
+
+import javax.microedition.lcdui.Canvas;
+import javax.microedition.lcdui.Graphics;
+
+public class StartGame extends Canvas implements Runnable{
+
+	public void run() {	
+	}
+        
+	protected void paint(Graphics graf) {
+            graf.setColor(255, 255, 255);
+            graf.fillRect(0, 0, getWidth(), getHeight());
+            graf.setColor(220, 68, 37); 
+            graf.drawString("Crabber", getWidth() / 2, getHeight() / 2, Graphics.HCENTER | Graphics.BASELINE);
+            graf.drawString("Maciej Kardas, Mateusz Gajewski", 1, getHeight(), Graphics.BOTTOM | Graphics.LEFT);
+	}
+}
